87b37f135f663de42e90c2c1588282558a07cea4
Tithiwat <tithiwattong@gmail.com>
2020-07-19 23:22:14 +0700

    14

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -15,7 +15,7 @@ gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
 # docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
-gcloud docker push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
+gcloud docker -- push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:latest
 
