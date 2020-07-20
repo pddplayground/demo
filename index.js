1a2eb2c1380d09c92ed35e6edf92fd2e2e77d033
Tithiwat <tithiwattong@gmail.com>
2020-07-20 09:20:17 +0700

    22

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World from ${hostname}`);
+  res.send(`Hello World from [ ${hostname} ]`);
 });
 
 app.listen(PORT, HOST);
